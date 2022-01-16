32def59322fe17e1d849bdbb7da009306a908935
Bo Bennetsen <Bo.Bennetsen@LEGO.com>
2022-01-16 14:04:41 +0100

    higher button in progress

--- game.js
+++ game.js
@@ -14,50 +14,59 @@ let userInputMax = 0; //highest number selected by user
    
   }
 
+  document.getElementById('level-button1').onclick = () => {
+
+    document.getElementById('level-button2').checked = false;
+    document.getElementById('level-button3').checked = false;
+
+      userInputMax = 50;
+
+  };
+
+  document.getElementById('level-button2').onclick = () => {
+
+    document.getElementById('level-button1').checked = false;
+    document.getElementById('level-button3').checked = false;
+
+      userInputMax = 25;
+    
+        };
+
+   document.getElementById('level-button3').onclick = () => {
+
+    document.getElementById('level-button1').checked = false;
+    document.getElementById('level-button2').checked = false;
+
+      userInputMax = 10;
+        
+            };
+
   
  window.onload = () => {
     document.getElementById('start-button').onclick = () => {
   
         let randomNumber = getRandomInt(userInputMin, userInputMax)
         
-        const gameBoard = document.getElementById("game-board");
-        const div = document.createElement("div");
+        let gameBoard = document.getElementById("game-board");
+        let div = document.createElement("div");
         gameBoard.appendChild(div)
-        gameBoard.innerHTML = `<div>${randomNumber}</div>`;
-
-        console.log(randomNumber)
-        
+        gameBoard.innerHTML = `<div id=random-number>${randomNumber}</div>`
     };
-
-    document.getElementById('level-button1').onclick = () => {
-
-      document.getElementById('level-button2').checked = false;
-      document.getElementById('level-button3').checked = false;
-
-        userInputMax = 50;
-
-    };
-
-    document.getElementById('level-button2').onclick = () => {
-
-      document.getElementById('level-button1').checked = false;
-      document.getElementById('level-button3').checked = false;
-
-        userInputMax = 25;
-      
-          };
-
-          document.getElementById('level-button3').onclick = () => {
-
-            document.getElementById('level-button1').checked = false;
-            document.getElementById('level-button2').checked = false;
-
-            userInputMax = 10;
-          
-              };
-}
-
-
-
+    
+  }
  
+ // Iteration 1: Example of a click event listener on `<button class="btn btn-pepperoni">`
+ function higherNumber() {
 
+  const randomNumber = getRandomInt(userInputMin, userInputMax)
+
+ const newNumber = document.getElementById('random-number')
+ newNumber.innerHTML = `${randomNumber}`
+
+
+  console.log(newNumber)
+
+  
+
+
+}
