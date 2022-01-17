6231d2ac8c3669c9cd39a88d61cbcb4ee0f90c7b
Bo Bennetsen <Bo.Bennetsen@LEGO.com>
2022-01-17 17:03:20 +0100

    score counter completed + code clean up

--- game.js
+++ game.js
@@ -4,86 +4,93 @@
 
 let userInputMin = 1; //lowest number
 let userInputMax = 0; //highest number
-
-document.getElementById('level-button1').onclick = () => {
-
-  document.getElementById('level-button2').checked = false;
-  document.getElementById('level-button3').checked = false;
-
-  const gameBoard = document.getElementById("game-board");
-
-  gameBoard.innerHTML = '';
-  
-
-    userInputMax = 50;
-
-};
-
-document.getElementById('level-button2').onclick = () => {
-
-  document.getElementById('level-button1').checked = false;
-  document.getElementById('level-button3').checked = false;
-
-  const gameBoard = document.getElementById("game-board");
-
-  gameBoard.innerHTML = '';
-
-    userInputMax = 25;
-  
-      };
-
- document.getElementById('level-button3').onclick = () => {
-
-  document.getElementById('level-button1').checked = false;
-  document.getElementById('level-button2').checked = false;
-
-  const gameBoard = document.getElementById("game-board");
-
-  gameBoard.innerHTML = '';
-
-    userInputMax = 10;
-      
-          };
-
-
 let correctAnswers = 0;
 let wrongAnswers = 0;
-let usedAnswers = 0; //to cancel game after too many wrong answers 
 let randomNumber = 0; 
 let prevNumber = 0; 
-let score;
+let score; //true or false
 
+let levelEasyButton = document.getElementById('level-button1');
+let levelMediumButton = document.getElementById('level-button2')
+let levelHardButton = document.getElementById('level-button3');
+let startButton = document.getElementById("start-button");
 
- function getRandomInt(min, max) {
+levelEasyButton.onclick = () => {
+
+  levelMediumButton.checked = false
+  levelHardButton.checked = false
+  startButton.disabled = false
+  
+  const gameBoard = document.getElementById("game-board");
+  gameBoard.innerHTML = '';
+  correctAnswers = 0;
+  wrongAnswers = 0;
+
+    userInputMax = 50;
+};
+
+levelMediumButton.onclick = () => {
+
+  levelEasyButton.checked = false
+  levelHardButton.checked = false
+  startButton.disabled = false
+
+  const gameBoard = document.getElementById("game-board");
+  gameBoard.innerHTML = '';
+  correctAnswers = 0;
+  wrongAnswers = 0;
+
+    userInputMax = 25;
+};
+
+ levelHardButton.onclick = () => {
+
+  levelEasyButton.checked = false
+  levelMediumButton.checked = false
+  startButton.disabled = false
+
+  const gameBoard = document.getElementById("game-board");
+
+  gameBoard.innerHTML = '';
+  correctAnswers = 0;
+  wrongAnswers = 0;
+
+  userInputMax = 10;
+    
+      
+};
+
+ function getRandomInt(min, max, prevNumber) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-  }
+    let randomNumber = prevNumber;
+    
+    while (prevNumber === randomNumber) {
+      randomNumber = Math.floor(Math.random() * (max - min) + min); 
+    }
+   return randomNumber;
+   }
 
- // Start the gamee
-function startGame() {
+ // Start the game 
+  document.querySelector('#start-button').addEventListener('click', function () {
   const gameBoard = document.getElementById("game-board");
   const div = document.createElement("div");
   gameBoard.appendChild(div)
   randomNumber = Math.floor(userInputMax / 2)
   gameBoard.innerHTML = `<div id=random-number>${randomNumber}</div>`
-
- // console.log(prevNumber)
-
- }
+ });
 
 //Get the next number
- function getNumber() {
-   prevNumber = randomNumber
- randomNumber = getRandomInt(userInputMin, userInputMax)
- const newNumber = document.getElementById('random-number')
- newNumber.innerHTML = `${randomNumber}`
-
+ function writeNextNumber() {
+  prevNumber = randomNumber
+  
+  randomNumber = getRandomInt(userInputMin, userInputMax, prevNumber)
+  const newNumber = document.getElementById('random-number')
+  newNumber.innerHTML = `${randomNumber}`
 }
 
-
-
   document.querySelector('#gamebutton-higher').addEventListener('click', function () {
+    writeNextNumber();
     console.log(prevNumber)
     console.log(randomNumber)
 
@@ -99,6 +106,7 @@ function startGame() {
 
 
   document.querySelector('#gamebutton-lower').addEventListener('click', function () {
+    writeNextNumber()
     console.log(prevNumber)
     console.log(randomNumber)
 
@@ -114,7 +122,6 @@ function startGame() {
   
 
 function calcScore(){
- 
 switch (score) {
   case true:
     correctAnswers += 1;
@@ -125,5 +132,29 @@ switch (score) {
 }
 console.log(correctAnswers)
 console.log(wrongAnswers)
-  
+result()
+}
+
+function result(){
+  if(correctAnswers === 5){
+    const result = document.getElementById('random-number')
+    result.innerHTML = 'YOU WON THE GAME'
+    console.log('YOU WON THE GAME')
+
+    setTimeout(endGame, 1000);
+  }
+   else if(wrongAnswers === 5){
+    const result = document.getElementById('random-number')
+    result.innerHTML = 'YOU LOST'
+    console.log('YOU LOST')
+
+    setTimeout(endGame, 1000);
+  }
+}
+
+function endGame(){
+
+alert(`You got ${correctAnswers} correct and ${wrongAnswers} wrong`);
+document.location.reload();
+clearInterval(interval); // Needed for Chrome to end game
 }
