2af4ea496ea552faddf867b40e211796da5e65ce
Bo Bennetsen <Bo.Bennetsen@LEGO.com>
2022-01-17 11:19:19 +0100

    score count working

--- game.js
+++ game.js
@@ -1,72 +1,129 @@
 
+
+
+
+let userInputMin = 1; //lowest number
+let userInputMax = 0; //highest number
+
+document.getElementById('level-button1').onclick = () => {
+
+  document.getElementById('level-button2').checked = false;
+  document.getElementById('level-button3').checked = false;
+
+  const gameBoard = document.getElementById("game-board");
+
+  gameBoard.innerHTML = '';
+  
+
+    userInputMax = 50;
+
+};
+
+document.getElementById('level-button2').onclick = () => {
+
+  document.getElementById('level-button1').checked = false;
+  document.getElementById('level-button3').checked = false;
+
+  const gameBoard = document.getElementById("game-board");
+
+  gameBoard.innerHTML = '';
+
+    userInputMax = 25;
+  
+      };
+
+ document.getElementById('level-button3').onclick = () => {
+
+  document.getElementById('level-button1').checked = false;
+  document.getElementById('level-button2').checked = false;
+
+  const gameBoard = document.getElementById("game-board");
+
+  gameBoard.innerHTML = '';
+
+    userInputMax = 10;
+      
+          };
+
+
 let correctAnswers = 0;
 let wrongAnswers = 0;
-let usedAnswers = 0; //to cancel game after too many wrong answers
-
-let userInputMin = 1; //lowest number selected by user
-let userInputMax = 0; //highest number selected by user
+let usedAnswers = 0; //to cancel game after too many wrong answers 
+let randomNumber = 0; 
+let prevNumber = 0; 
+let score;
 
 
  function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-   
   }
 
-  document.getElementById('level-button1').onclick = () => {
+ // Start the gamee
+function startGame() {
+  const gameBoard = document.getElementById("game-board");
+  const div = document.createElement("div");
+  gameBoard.appendChild(div)
+  randomNumber = Math.floor(userInputMax / 2)
+  gameBoard.innerHTML = `<div id=random-number>${randomNumber}</div>`
 
-    document.getElementById('level-button2').checked = false;
-    document.getElementById('level-button3').checked = false;
+ // console.log(prevNumber)
 
-      userInputMax = 50;
+ }
 
-  };
-
-  document.getElementById('level-button2').onclick = () => {
-
-    document.getElementById('level-button1').checked = false;
-    document.getElementById('level-button3').checked = false;
-
-      userInputMax = 25;
-    
-        };
-
-   document.getElementById('level-button3').onclick = () => {
-
-    document.getElementById('level-button1').checked = false;
-    document.getElementById('level-button2').checked = false;
-
-      userInputMax = 10;
-        
-            };
-
-  
- window.onload = () => {
-    document.getElementById('start-button').onclick = () => {
-  
-        let randomNumber = getRandomInt(userInputMin, userInputMax)
-        
-        let gameBoard = document.getElementById("game-board");
-        let div = document.createElement("div");
-        gameBoard.appendChild(div)
-        gameBoard.innerHTML = `<div id=random-number>${randomNumber}</div>`
-    };
-    
-  }
- 
- // Iteration 1: Example of a click event listener on `<button class="btn btn-pepperoni">`
- function higherNumber() {
-
-  const randomNumber = getRandomInt(userInputMin, userInputMax)
-
+//Get the next number
+ function getNumber() {
+   prevNumber = randomNumber
+ randomNumber = getRandomInt(userInputMin, userInputMax)
  const newNumber = document.getElementById('random-number')
  newNumber.innerHTML = `${randomNumber}`
 
+}
 
-  console.log(newNumber)
 
+
+  document.querySelector('#gamebutton-higher').addEventListener('click', function () {
+    console.log(prevNumber)
+    console.log(randomNumber)
+
+    if(prevNumber < randomNumber){
+      score = true;
+     }
+     else if(prevNumber > randomNumber){
+       score = false;
+     }
+     console.log(score)
+     calcScore()
+  });
+
+
+  document.querySelector('#gamebutton-lower').addEventListener('click', function () {
+    console.log(prevNumber)
+    console.log(randomNumber)
+
+    if(prevNumber < randomNumber){
+      score = false;
+     }
+     else if(prevNumber > randomNumber){
+       score = true;
+     }
+     console.log(score)
+     calcScore()
+  });
   
 
-
+function calcScore(){
+ 
+switch (score) {
+  case true:
+    correctAnswers += 1;
+    break;
+  case false:
+    wrongAnswers += 1;
+    break;
+}
+console.log(correctAnswers)
+console.log(wrongAnswers)
+  
 }
